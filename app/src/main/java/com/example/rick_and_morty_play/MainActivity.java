97ca1e666832b279777e688add152a968f70f7a2
Leonardo Rick <leonardo@lanytecnologia.com.br>
2020-06-15 00:21:26 -0300

    change icon; set up video play

--- app/src/main/java/com/example/rick_and_morty_play/MainActivity.java
+++ app/src/main/java/com/example/rick_and_morty_play/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.rick_and_morty_play;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,7 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
+
+    public void openVideo(View view) {
+        startActivity(new Intent(this, PlayerActivity.class));
+    }
+
 
 }
--- app/src/main/java/com/example/rick_and_morty_play/PlayerActivity.java
+++ app/src/main/java/com/example/rick_and_morty_play/PlayerActivity.java
@@ -1,13 +1,37 @@
 package com.example.rick_and_morty_play;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class PlayerActivity extends AppCompatActivity {
 
+    private VideoView videoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        // Hide ActionBar
+        getSupportActionBar().hide();
+
+        // Hide native StatusBar and NavigationBar
+        View decorView = getWindow().getDecorView(); // Returns object that allows screen manipulation
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN); // Set FullScreen flag to screen manipulation
+
+
+        // Play Video
+        videoView = findViewById(R.id.videoView);
+        videoView.setMediaController(new MediaController(this)); // mediaController config (play, pause, time...)
+        videoView.setVideoPath(
+            "android.resource://" + // Default android prefix to access resources
+            getPackageName() + // Find the package name defined after (ex: R.raw.video)
+            "/" +
+            R.raw.video // Where's my .mp4
+        );
+        videoView.start();
     }
 }
