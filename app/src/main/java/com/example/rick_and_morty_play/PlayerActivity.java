9446d123e5391ef308417d7acf42d83991b9c112
Leonardo Rick <leonardo@lanytecnologia.com.br>
2020-06-15 00:10:54 -0300

    final configuration

---
+++ app/src/main/java/com/example/rick_and_morty_play/PlayerActivity.java
@@ -0,0 +1,13 @@
+package com.example.rick_and_morty_play;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
